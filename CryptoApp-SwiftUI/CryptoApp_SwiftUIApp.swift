2ed57b99881c0bcbfdc9441ebedab6ae8c2eb3f0
AGorkemAksoz <a.gorkemaksoz@gmail.com>
2024-01-16 14:25:53 +0300

    Initial Commit

---
+++ CryptoApp-SwiftUI/CryptoApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp_SwiftUIApp.swift
+//  CryptoApp-SwiftUI
+//
+//  Created by Gorkem on 16.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
